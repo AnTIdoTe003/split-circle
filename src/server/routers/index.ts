721f05e463743f240518703ac606459cdf9474e0
Debmalya Biswas <viperbale.db@gmail.com>
2025-02-22 21:20:29 +0530

    changes

--- src/server/routers/index.ts
+++
@@ -1,8 +0,0 @@
-import { trpc } from '../trpc';
-import { authRouter } from './authRouter';
-
-export const appRouter = trpc.router({
-    auth: authRouter,
-});
-
-export type AppRouter = typeof appRouter;
